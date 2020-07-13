6cf6b51036de0493c6116d44194fb6cca82f0254
ooppitz <ooppitz@gmail.com>
2020-07-13 14:00:26 +0200

    Refactoring.

--- src/main/java/de/azubiag/MassnahmenBewertung/App.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/App.java
@@ -51,27 +51,18 @@ public class App extends Application {
 	// TODO: Remove throw Excpetions, wenn Benedikt das Exception-Handling
 	// korrigiert hat
 
+	static String[] cipherStrings = {
+			"AU2FsdGVkX190o2DknyWMHa9XGq2JKwmap0zvJd0c39jbzkEqRC44FQ34TGGs2wETooOKw1nIwnbk31R+o4Tg3k/h5fa/BAeZJeC9Y1DHqECvZWlljvv5lOHJhD1L4/TnZb3Ks2cMJE1eky+8BchhrkvMB1zAqhOVb8vKw7ov3t23eEGm+o3/yT2V27EH+gizutaSy9BU/X/kXTVpncxi4hlCNXR8p0mJQRRj4oKZy+19jAGqYt+grJqUlTir+zXjs6jP+QjCPFhFdfQZQ2IYbA==",
+			"AU2FsdGVkX184mE21H5Jv3ktZys9mIF5HfvrFw9Od8gTDZe5TljcoItmuKmY3MPVUO4043uRkyPQc+btvCxnob5aCq+O2PQok2nqwkZ37nny5Wp9dApfNG79dIweKjqWM38P6Qkmn+iJ0yKeCZrtHT4HnztL02JlE0nAxBtHS5Nf2gM42u6vZhseqn5E3o9opJg/hUYoSxUa6ZwhmFlb74g==",
+			"AU2FsdGVkX18Hfx/uQqsOVkTmCzj89C+n7ChAgoh69rrPyJhNYzGdX79uOg0te+hcGHnzIgztVX06m1av5qoXiO7S9VI1Te7FE/93jg43FaNz3zIG4M/BkyRWbmCqLre4VR1eMHTyZrvyc6rWrc3eqHRCZRicDgdMLJzBCswPrx+S3hOAzURnrlCn84e7JaisYLuAl9Y5qRLOZHCtF6Hl3v3r+qc0Z+vjT5LpUlzkkto=" };
+
 	static String berechneAuswertung() {
 
 		String ergebnis = "";
 
-		// Benedikt
-		// Input von verschlüsselten Strings -> Branch von Benedikt
-		// entschlüsseln der String
-
 		ArrayList<String> klarTextStrings = new ArrayList<String>();
 
-		String[] klarText = {
-				"3|1|Alles war toll|4|Das war super toll|Es war klasse!|Robert Hackfuß|4|4|4|4|0|Er macht mir Angst!|Franz Karrenschlepper|2|0|0|0|4||",
-				"4|4||0|Gr8 b8 m8 r8 8/8|Werf Sie raus!!!|Robert Hackfuß|1|2|1|0|3||Franz Karrenschlepper|0|1|1|0|0|Er hätte beim Karrenschleppen bleiben sollen.",
-				"0|4|Nö|3||Lass mich in Ruhe|Robert Hackfuß|3|1|2|2|0|Warum trägt er immer ein Hackebeil?|Franz Karrenschlepper|3|1|0|0|3|Seine Augen sind immer weit offen.Das ist unheimlich!|" };
-
-		String[] cipherString = {
-				"AU2FsdGVkX190o2DknyWMHa9XGq2JKwmap0zvJd0c39jbzkEqRC44FQ34TGGs2wETooOKw1nIwnbk31R+o4Tg3k/h5fa/BAeZJeC9Y1DHqECvZWlljvv5lOHJhD1L4/TnZb3Ks2cMJE1eky+8BchhrkvMB1zAqhOVb8vKw7ov3t23eEGm+o3/yT2V27EH+gizutaSy9BU/X/kXTVpncxi4hlCNXR8p0mJQRRj4oKZy+19jAGqYt+grJqUlTir+zXjs6jP+QjCPFhFdfQZQ2IYbA==",
-				"AU2FsdGVkX184mE21H5Jv3ktZys9mIF5HfvrFw9Od8gTDZe5TljcoItmuKmY3MPVUO4043uRkyPQc+btvCxnob5aCq+O2PQok2nqwkZ37nny5Wp9dApfNG79dIweKjqWM38P6Qkmn+iJ0yKeCZrtHT4HnztL02JlE0nAxBtHS5Nf2gM42u6vZhseqn5E3o9opJg/hUYoSxUa6ZwhmFlb74g==",
-				"AU2FsdGVkX18Hfx/uQqsOVkTmCzj89C+n7ChAgoh69rrPyJhNYzGdX79uOg0te+hcGHnzIgztVX06m1av5qoXiO7S9VI1Te7FE/93jg43FaNz3zIG4M/BkyRWbmCqLre4VR1eMHTyZrvyc6rWrc3eqHRCZRicDgdMLJzBCswPrx+S3hOAzURnrlCn84e7JaisYLuAl9Y5qRLOZHCtF6Hl3v3r+qc0Z+vjT5LpUlzkkto=" };
-
-		for (String s : cipherString) {
+		for (String s : cipherStrings) {
 
 			try {
 
