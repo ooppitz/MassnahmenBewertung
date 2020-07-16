ee25f42b035eb2f25989c368e3da7486cf8ae8f3
filip-golanski <filip.golanski@googlemail.com>
2020-07-16 15:33:07 +0200

    Auslagern des neuen Ordners in eine Methode

--- src/main/java/de/azubiag/MassnahmenBewertung/UI/ControllerLogin.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/UI/ControllerLogin.java
@@ -47,57 +47,67 @@ public class ControllerLogin {
 				String clean_username = username.getText();	// bestimmte Zeichen aus Nutzereingabe entfernen?
 				// schauen, ob das Feld nicht leer ist
 				// Auswahlliste von Namen davor anzeigen
-				String path;
-				try {
-					path = Upload.getInstance().getRepositoryPfad()+"fragebogen\\"+clean_username;
-				} catch (Exception e2) {
-					return; // besser handlen?
-				}
-				System.out.println(path);
-				//				Path weg = Paths.get();
-				File test_file = new File(path);
-				System.out.println("Exists:\t"+test_file.exists());
-				System.out.println("Is Directory:\t"+test_file.isDirectory());
+				boolean alles_ok = erstelleNeuenOrdnerFallsNotwendig(clean_username);
 
-				if(!test_file.isDirectory() )
+				if(alles_ok)
 				{
-					if(test_file.exists())
-					{
-						Alert error = new Alert(AlertType.ERROR);
-						error.setTitle("Eine Datei mit diesem Namen existiert leider schon!");
-						error.setHeaderText("Eine Datei mit diesem Namen existiert leider schon!");
-						ButtonType end = new ButtonType("OK", ButtonData.CANCEL_CLOSE);
-						error.getButtonTypes().setAll(end);
-						error.show();
-						return;
-					}
-					else
-					{
-						boolean success = test_file.mkdir();
-						if (!success)
-						{
-							System.out.println("Directory could not be created!!!");
-
-							// Alert
-							Alert error = new Alert(AlertType.ERROR);
-							error.setTitle("Dieser Benutzername kann nicht verwendet werden!");
-							error.setHeaderText("Dieser Benutzername kann nicht verwendet werden!");
-							ButtonType end = new ButtonType("OK", ButtonData.CANCEL_CLOSE);
-							error.getButtonTypes().setAll(end);
-							error.show();
-							return;
-						}
-						else
-						{
-							System.out.println("Directory created!");
-						}
-					}
-
+					mainapp.showTabPane();
 				}
-
-				mainapp.showTabPane();
-
 			}
+
 		});
+	}
+
+	private boolean erstelleNeuenOrdnerFallsNotwendig(String clean_username) {
+
+		String path;
+		try {
+			path = Upload.getInstance().getRepositoryPfad()+"fragebogen\\"+clean_username;
+		} catch (Exception e2) {
+			return false; // besser handlen?
+		}
+		System.out.println(path);
+
+		File test_file = new File(path);
+		System.out.println("Exists:\t"+test_file.exists());
+		System.out.println("Is Directory:\t"+test_file.isDirectory());
+
+		if(!test_file.isDirectory() )
+		{
+			if(test_file.exists())
+			{
+				Alert error = new Alert(AlertType.ERROR);
+				error.setTitle("Eine Datei mit diesem Namen existiert leider schon!");
+				error.setHeaderText("Eine Datei mit diesem Namen existiert leider schon!");
+				ButtonType end = new ButtonType("OK", ButtonData.CANCEL_CLOSE);
+				error.getButtonTypes().setAll(end);
+				error.show();
+				return false;
+			}
+			else
+			{
+				boolean success = test_file.mkdir();
+				if (!success)
+				{
+					System.out.println("Directory could not be created!!!");
+
+					// Alert
+					Alert error = new Alert(AlertType.ERROR);
+					error.setTitle("Dieser Benutzername kann nicht verwendet werden!");
+					error.setHeaderText("Dieser Benutzername kann nicht verwendet werden!");
+					ButtonType end = new ButtonType("OK", ButtonData.CANCEL_CLOSE);
+					error.getButtonTypes().setAll(end);
+					error.show();
+					return false;
+				}
+				else
+				{
+					System.out.println("Directory created!");
+					return true;
+				}
+			}
+
+		}
+		return true;
 	}
 }
