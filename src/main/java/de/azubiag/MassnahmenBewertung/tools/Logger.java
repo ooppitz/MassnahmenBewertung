39fc2aa99c776352cee7ce0ef7454d06135b49ac
ooppitz <ooppitz@gmail.com>
2020-07-20 10:41:40 +0200

    Verbesserung am Logger.

--- src/main/java/de/azubiag/MassnahmenBewertung/tools/Logger.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/tools/Logger.java
@@ -2,6 +2,19 @@ package de.azubiag.MassnahmenBewertung.tools;
 
 public class Logger {
 
+	static Logger theLogger; 
+	
+	public static Logger getLogger() {
+		if (theLogger == null) {
+			theLogger = new Logger();
+		}
+		return  theLogger;
+	}
+	
+	public static void log(String logMessage) {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
