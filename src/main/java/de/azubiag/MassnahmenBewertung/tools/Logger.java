4d1e48da49ea785aacba15d9f244cd8386ce6734
ooppitz <ooppitz@gmail.com>
2020-07-20 10:43:12 +0200

    Kommentar über die Funktionen der Klasse.

--- src/main/java/de/azubiag/MassnahmenBewertung/tools/Logger.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/tools/Logger.java
@@ -1,5 +1,27 @@
 package de.azubiag.MassnahmenBewertung.tools;
 
+/*
+
+Fehlermeldung und Stackbacktrace automatisch in Log-File schreiben 
+
+Michael: 
+- Die tatsächlich geworfene Exception anzeigen 
+- StringWriter, PrintWriter um die Methode printStacktTrace letztlich in ein File schreiben 
+- getClass und GetMessage
+
+Log-Datei : kann man auch ignorieren, man kann den Anwender bitten, die Datei zu schicken
+
+1. Anlegen der Logdatei im Repository
+2. Committen und Pushen des Logfiles
+3. Anzeigen einer Fehlermeldung mit Hinweis, die AzubiAG zu informieren
+4. Inhalt des Logfiles
+   - Uhrzeit, Datum
+   - Stackbacktrace
+   - "Alles", was angeklickt wird bzw. zu erfassen ist
+
+ */
+
+
 public class Logger {
 
 	static Logger theLogger; 
@@ -10,6 +32,8 @@ public class Logger {
 		}
 		return  theLogger;
 	}
+	
+	/* Wird von der GUI aufgerufen, um relevante Daten zu loggen */
 	
 	public static void log(String logMessage) {
 		
