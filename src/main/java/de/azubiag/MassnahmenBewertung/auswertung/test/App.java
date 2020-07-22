f10fce71879e420066b34aac89472e9c9a01d5a2
ooppitz <ooppitz@gmail.com>
2020-07-22 13:34:14 +0200

    Hinzufügen des Package de.azubiag.MassnahmenBewertung.auswertung.test und einer Test-App.

---
+++ src/main/java/de/azubiag/MassnahmenBewertung/auswertung/test/App.java
@@ -0,0 +1,15 @@
+package de.azubiag.MassnahmenBewertung.auswertung.test;
+
+public class App {
+
+	
+	/** Liest Antwort-Strings (verschlüsselt oder unverschlüsselt )aus einem File 
+	 * oder von der Kommandozeile und verarbeitet sie.
+	 * Der Output sollte eine komplette Auswertung sein. 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
