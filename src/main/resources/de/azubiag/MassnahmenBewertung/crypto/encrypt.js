edbe909c6ea7b979e0acbadb846d79534e0dd6e8
Benedikt-Greinwald <benedikt.greinwald@gmail.com>
2020-07-22 10:28:31 +0200

    option "C" ohne verschluesselung hinzugefügt

--- src/main/resources/de/azubiag/MassnahmenBewertung/crypto/encrypt.js
+++ src/main/resources/de/azubiag/MassnahmenBewertung/crypto/encrypt.js
@@ -1,28 +1,31 @@
 // DO NOT USE THIS / ONLY FOR TESTING
 
 
-var text = "!§$%&/()²[]³@~+*<>|-#=?´öä_.:s°^";
+let text = "Hello!";
 
 // type A = AES; type B = XOR
 function encrypt(str, type) {
     switch (type) {
         case "A":
             // Key
-            var secret = "Jupiter";
-            var encrypted = CryptoJS.AES.encrypt(str, secret);
+            let secret = "Jupiter";
+            let encrypted = CryptoJS.AES.encrypt(str, secret);
             encrypted = encrypted.toString();
             return "A" + encrypted;
         case "B":
-            var encoded = "";
+            let encoded = "";
             for (i = 0; i < str.length; i++) {
-                var a = str.charCodeAt(i);
-                var b = a ^ 1;
+                let a = str.charCodeAt(i);
+                let b = a ^ 1;
                 encoded = encoded + String.fromCharCode(b);
             }
             return "B" + encoded;
+        case "C":
+            return "C" + str;
     }
 }
 
 console.log("Cipher text A: " + encrypt(text, "A"));
 console.log("Cipher text B: " + encrypt(text, "B"));
+console.log("Cipher text C: " + encrypt(text, "C"));
 document.getElementById("output").innerHTML = "Output in Console";
