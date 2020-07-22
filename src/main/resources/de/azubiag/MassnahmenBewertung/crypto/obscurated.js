405b9f7273f4a396e3e74ab43e045b7c9d12dc5c
Benedikt-Greinwald <benedikt.greinwald@gmail.com>
2020-07-22 10:53:45 +0200

    deleted javascript crypto files

--- src/main/resources/de/azubiag/MassnahmenBewertung/crypto/obscurated.js
+++
@@ -1,9 +0,0 @@
-// function encrypt(text to encrypt, type (A, B, C))
-let text = "Hello World!";
-
-let _0xfcd2=["\x4A\x75\x70\x69\x74\x65\x72","\x65\x6E\x63\x72\x79\x70\x74","\x41\x45\x53","\x41","","\x6C\x65\x6E\x67\x74\x68","\x63\x68\x61\x72\x43\x6F\x64\x65\x41\x74","\x66\x72\x6F\x6D\x43\x68\x61\x72\x43\x6F\x64\x65","\x42","\x43"];function encrypt(_0x35d9x2,_0x35d9x3){switch(_0x35d9x3){case _0xfcd2[3]:let _0x35d9x4=_0xfcd2[0];let _0x35d9x5=CryptoJS[_0xfcd2[2]][_0xfcd2[1]](_0x35d9x2,_0x35d9x4);_0x35d9x5= _0x35d9x5.toString();return _0xfcd2[3]+ _0x35d9x5;case _0xfcd2[8]:let _0x35d9x6=_0xfcd2[4];for(i= 0;i< _0x35d9x2[_0xfcd2[5]];i++){let _0x35d9x7=_0x35d9x2[_0xfcd2[6]](i);let _0x35d9x8=_0x35d9x7^ 1;_0x35d9x6= _0x35d9x6+ String[_0xfcd2[7]](_0x35d9x8)};return _0xfcd2[8]+ _0x35d9x6;case _0xfcd2[9]:return _0xfcd2[9]+ _0x35d9x2}}
-
-console.log("Cipher text A: " + encrypt(text, "A"));
-console.log("Cipher text B: " + encrypt(text, "B"));
-console.log("Cipher text C: " + encrypt(text, "C"));
-document.getElementById("output").innerHTML = "Output in Console";
