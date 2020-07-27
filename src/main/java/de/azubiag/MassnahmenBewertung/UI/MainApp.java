61c8aea1ec29f921c95030dccf03a4215c7d6175
Luna <lunadragofelis@gmail.com>
2020-07-27 10:58:25 +0200

    Testmode-Flag aktiviert

--- src/main/java/de/azubiag/MassnahmenBewertung/UI/MainApp.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/UI/MainApp.java
@@ -300,6 +300,11 @@ public class MainApp extends Application {
 	}
 
 	public static void main(String[] args) {
+		
+		for (int i=0; i<args.length; i++) {
+			if (args[i].equals("--test")) setTestmodusAktiv(true);
+		}
+		
 		launch(args);
 	}
 	
