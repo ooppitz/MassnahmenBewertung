10381d4ebfd5bd903ceb015121a33cdfe7315f40
Luna <lunadragofelis@gmail.com>
2020-07-28 10:04:20 +0200

    Zwischencommit

--- src/main/java/de/azubiag/MassnahmenBewertung/UI/MultiAntwortParser.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/UI/MultiAntwortParser.java
@@ -10,9 +10,50 @@ public class MultiAntwortParser {
 	}
 	
 	public static ArrayList<String> parse(String fromClipboard) {
-		var result = new ArrayList<String>();
-		
-		return result;
+		return new MultiAntwortParser(fromClipboard).doTheParsing();
+	}
+	
+	private String source;
+	
+	private MultiAntwortParser(String raw) {
+		this.source = raw;
+	}
+	
+	private int minuses;
+	private boolean inGap;
+	private int strindex;
+	private boolean loopOn;
+	
+	private StringBuilder sb;
+	
+	private static final char MINUS = '-';
+	private static final int MINUSES_REQUIRED = 3;
+	
+	private ArrayList<String> buffer;
+	
+	private ArrayList<String> doTheParsing() {
+		init();
+		while(loop());
+		return buffer;
+	}
+
+	private boolean loop() {
+		loopEndActions();
+		return loopOn;
+	}
+
+	private void loopEndActions() {
+		strindex++;
+		if (strindex >= source.length()) loopOn = false;
+	}
+
+	private void init() {
+		loopOn = true;
+		minuses = 0;
+		buffer = new ArrayList<>();
+		sb = new StringBuilder();
+		inGap = true;
+		strindex = 0;
 	}
 
 }
