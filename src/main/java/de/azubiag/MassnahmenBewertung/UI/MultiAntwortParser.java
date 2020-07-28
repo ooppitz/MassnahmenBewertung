ae05abb30f21f80f810b1ccd381f4cd227057711
Luna <lunadragofelis@gmail.com>
2020-07-28 09:37:15 +0200

    Grundstruktur von MultiAntwortParser angelegt

---
+++ src/main/java/de/azubiag/MassnahmenBewertung/UI/MultiAntwortParser.java
@@ -0,0 +1,18 @@
+package de.azubiag.MassnahmenBewertung.UI;
+
+import java.util.ArrayList;
+
+public class MultiAntwortParser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static ArrayList<String> parse(String fromClipboard) {
+		var result = new ArrayList<String>();
+		
+		return result;
+	}
+
+}
