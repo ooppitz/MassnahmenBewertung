bca09284f715e7c07a1bca90520296ee6209487a
ooppitz <ooppitz@gmail.com>
2020-07-29 17:34:01 +0200

    Refactoring : Hinzufügen von Konstanten.

--- src/main/java/de/azubiag/MassnahmenBewertung/datenstrukturen/AzubiAntwort.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/datenstrukturen/AzubiAntwort.java
@@ -28,21 +28,35 @@ public class AzubiAntwort implements Serializable{
 	
 	public String verschlüsselterString;
 	
-	/** Erzeugt ein AzubiAntwort-Objekt.
+	
+	final static int IDX_UMFRAGEID = 0;
+	final static int IDX_ANTWORTID = 1;
+	final static int IDX_MASSNAHME_BEGINN = 2; 
+	final static int IDX_REFERENTEN_BEGINN = 8;
+	final static int COUNT_MASSNAHME_STRINGS = 6;
+	final static int COUNT_REFERENT_STRINGS = 7;
+	
+	/** Erzeugt aus dem kodierten String ein AzubiAntwort-Objekt und speichert den verschlüsselten String mit ab.
 	 * 
 	 * @param kodiert : Entschlüsselter String
 	 */
 	public AzubiAntwort(String kodiert, String verschlüsselt) {
+		
 		verschlüsselterString = verschlüsselt;
+		
 		String[] array = kodiert.split("\\|", -1); // -1 verhindert, dass split() leere Strings am Ende verwirft und dann das Array zu kurz ist
-		umfrageID = Integer.parseInt(array[0]);
-		antwortID = Integer.parseInt(array[1]);
-		massnahme = new BewertungMassnahme(subarray(array, 2, 8));
+		
+		umfrageID = Integer.parseInt(array[IDX_UMFRAGEID]);
+		
+		antwortID = Integer.parseInt(array[IDX_ANTWORTID]);
+		
+		massnahme = new BewertungMassnahme(subarray(array, IDX_MASSNAHME_BEGINN, IDX_MASSNAHME_BEGINN + COUNT_MASSNAHME_STRINGS));
+		
 		referenten = new ArrayList<>(10);
-		int index = 8;
-		while (index+6 < array.length) {
-			referenten.add(new BewertungReferent(subarray(array, index, index+7)));
-			index += 7;
+		int index = IDX_REFERENTEN_BEGINN;
+		while (index + COUNT_REFERENT_STRINGS - 1 < array.length) {
+			referenten.add(new BewertungReferent(subarray(array, index, index + COUNT_REFERENT_STRINGS)));
+			index += COUNT_REFERENT_STRINGS;
 		}
 	}
 	
@@ -63,9 +77,9 @@ public class AzubiAntwort implements Serializable{
 		}
 	}
 	
-	private static String[] subarray(String[] mainArray, int start, int end) {
+	private static String[] subarray(String[] inputArray, int start, int end) {
 		String[] newarray = new String[end-start];
-		System.arraycopy(mainArray, start, newarray, 0, end-start);
+		System.arraycopy(inputArray, start, newarray, 0, end-start);
 		return newarray;
 	}
 	
