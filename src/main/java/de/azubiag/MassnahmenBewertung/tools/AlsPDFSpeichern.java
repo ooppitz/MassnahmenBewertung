c76a063d2a4ba934d993bfd8eebdb07bca51cc01
Denis <bode.denis123@gmail.com>
2020-07-30 15:23:24 +0200

    Formatierte und eingerückte Version

--- src/main/java/de/azubiag/MassnahmenBewertung/tools/AlsPDFSpeichern.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/tools/AlsPDFSpeichern.java
@@ -54,183 +54,186 @@ public class AlsPDFSpeichern {
 				e.printStackTrace();
 			}
 			document.open();
-			
+
 			Font font = FontFactory.getFont(FontFactory.COURIER, 10, BaseColor.BLACK);
 			// Chunk chunk = new Chunk("Hello World", font);
 
 			// document.add(chunk);
-			
+
 			Paragraph header = new Paragraph("", font);
 			header.setSpacingAfter(50);
-			header.add(new Phrase("Maßnahme von "+fe.von_datum+" bis "+fe.bis_datum+"\n"));
-			header.add(new Phrase("Auftragsnummer: "+fe.auftrags_nummer+"\n"));
-			header.add(new Phrase("Seminarleitung: "+fe.seminarleiter_name+"\n"));
-			header.add(new Phrase("Datum: "+fe.ausstellungs_datum));
-			
+			header.add(new Phrase("Maßnahme von " + fe.von_datum + " bis " + fe.bis_datum + "\n"));
+			header.add(new Phrase("Auftragsnummer: " + fe.auftrags_nummer + "\n"));
+			header.add(new Phrase("Seminarleitung: " + fe.seminarleiter_name + "\n"));
+			header.add(new Phrase("Datum: " + fe.ausstellungs_datum));
+
 			document.add(header);
-			
+
 			Paragraph m1 = new Paragraph("", font);
 			m1.setSpacingAfter(50);
-			
+
 			Phrase title1 = new Phrase("1. Maßnahmenverlauf");
 			leerzeichenSetzen(title1);
 			title1.add(new Chunk("-2 -1  0  1  2   Ø\n"));
-			
+
 			Phrase t1q1 = new Phrase("Wie empfinden die Teilnehmer die Organisation des Seminars?");
 			leerzeichenSetzen(t1q1);
 			for (int i = 0; i < am.pktvertOrg.length; i++) {
-				t1q1.add(new Chunk(" "+String.valueOf(am.pktvertOrg[i])+" "));
-			}	
+				t1q1.add(new Chunk(" " + String.valueOf(am.pktvertOrg[i]) + " "));
+			}
 			t1q1.add("\n");
-			
+
 			Phrase t1q2 = new Phrase("Wie empfinden die Teilnehmer den Verlauf des Seminars?");
 			leerzeichenSetzen(t1q2);
 			for (int i = 0; i < am.pktvertOrg.length; i++) {
-				t1q2.add(new Chunk(" "+String.valueOf(am.pktvertVerl[i])+" "));
+				t1q2.add(new Chunk(" " + String.valueOf(am.pktvertVerl[i]) + " "));
 			}
 			t1q2.add("\n");
-			
+
 			Paragraph t1b = new Paragraph("Bemerkungen dazu:\n", font);
 			for (int i = 0; i < am.alleBemerkVerl.size(); i++) {
-				t1b.add(new Chunk(am.alleBemerkVerl.get(i)+"; "));
+				t1b.add(new Chunk(am.alleBemerkVerl.get(i) + "; "));
 			}
-			
+
 			m1.add(title1);
 			m1.add(t1q1);
 			m1.add(t1q2);
 			m1.add(t1b);
-			
+
 			document.add(m1);
-			
+
 			Paragraph m2 = new Paragraph();
 			m2.setSpacingAfter(50);
-			
-			Paragraph title2 = new Paragraph("2. Maßnahmebetreuung",font);
+
+			Paragraph title2 = new Paragraph("2. Maßnahmebetreuung", font);
 			leerzeichenSetzen(title2);
 			title2.add(new Chunk("-2 -1  0  1  2   Ø"));
-			
-			Paragraph t2q1 = new Paragraph("Wie zufrieden sind die Teilnehmer mit der Betreuung des BFZ?",font);
+
+			Paragraph t2q1 = new Paragraph("Wie zufrieden sind die Teilnehmer mit der Betreuung des BFZ?", font);
 			leerzeichenSetzen(t2q1);
 			for (int i = 0; i < am.pktvertBetrng.length; i++) {
-				t2q1.add(new Chunk(" "+String.valueOf(am.pktvertBetrng[i])+" "));
+				t2q1.add(new Chunk(" " + String.valueOf(am.pktvertBetrng[i]) + " "));
 			}
-			Paragraph t2b1 = new Paragraph("Bermerkungen dazu:\n",font);
+			Paragraph t2b1 = new Paragraph("Bermerkungen dazu:\n", font);
 			for (int i = 0; i < am.alleBemerkBetrng.size(); i++) {
-				t2b1.add(new Chunk(am.alleBemerkBetrng.get(i)+"; ",font));
+				t2b1.add(new Chunk(am.alleBemerkBetrng.get(i) + "; ", font));
 			}
-			
+
 			m2.add(title2);
 			m2.add(t2q1);
 			m2.add(t2b1);
-			
+
 			document.add(m2);
-			
-			Paragraph title3 = new Paragraph("3. Bewertung der Referenten bzw. Referentinnen:\n",font);
+
+			Paragraph title3 = new Paragraph("3. Bewertung der Referenten bzw. Referentinnen:\n", font);
 			for (int i = 0; i < am.alleBemerkRefAllg.size(); i++) {
-				title3.add(new Chunk(am.alleBemerkRefAllg.get(i)+"; ",font));
+				title3.add(new Chunk(am.alleBemerkRefAllg.get(i) + "; ", font));
 			}
 			title3.setSpacingAfter(50);
-			
+
 			document.add(title3);
-			
+
 			Paragraph title4 = new Paragraph("4. Auswertung der Referenten:", font);
 			leerzeichenSetzen(title4);
 			title4.add(new Chunk("-2 -1  0  1  2   Ø"));
-			
+
 			document.add(title4);
-			
+
 			Paragraph rAlle = new Paragraph("", font);
-			
+
 			Phrase rVorb = new Phrase();
 			Phrase rFach = new Phrase();
 			Phrase rEing = new Phrase();
 			Phrase rInh = new Phrase();
 			Phrase rVerh = new Phrase();
 			Phrase rBem = new Phrase();
-			
-			for(int i = 0; i<ar.size(); i++) {
-				rAlle.add(new Chunk(ar.get(i).name+"\n"));
-				
+
+			for (int i = 0; i < ar.size(); i++) {
+				rAlle.add(new Chunk(ar.get(i).name + "\n"));
+
 				rVorb.add(new Chunk("Wie war ihr/sein Unterricht vorbereitet?"));
 				leerzeichenSetzen(rVorb);
-				for(int j=0; j<ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
-					rVorb.add(new Chunk(" "+String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.VORBEREITUNG, j))+" "));	
-				}	
+				for (int j = 0; j < ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
+					rVorb.add(new Chunk(
+							" " + String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.VORBEREITUNG, j)) + " "));
+				}
 				rVorb.add("\n");
-				
+
 				rFach.add(new Chunk("Wie umfangreich war ihr/sein Fachwissen?"));
 				leerzeichenSetzen(rFach);
-				for(int j=0; j<ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
-					rFach.add(new Chunk(" "+String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.FACHWISSEN, j))+" "));	
-				}	
+				for (int j = 0; j < ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
+					rFach.add(new Chunk(
+							" " + String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.FACHWISSEN, j)) + " "));
+				}
 				rFach.add("\n");
-				
+
 				rEing.add(new Chunk("Wie ging sie/er auf spezielle thematische Probleme ein?"));
 				leerzeichenSetzen(rEing);
-				for(int j=0; j<ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
-					rEing.add(new Chunk(" "+String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.EINGEHENAUFPROBLEME, j))+" "));	
-				}	
+				for (int j = 0; j < ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
+					rEing.add(new Chunk(" "
+							+ String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.EINGEHENAUFPROBLEME, j)) + " "));
+				}
 				rEing.add("\n");
-				
+
 				rInh.add(new Chunk("Wie verständlich konnte sie/er die Inhalte vermitteln?"));
 				leerzeichenSetzen(rInh);
-				for(int j=0; j<ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
-					rInh.add(new Chunk(" "+String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.INHALTSVERMITTLUNG, j))+" "));	
-				}	
+				for (int j = 0; j < ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
+					rInh.add(new Chunk(" "
+							+ String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.INHALTSVERMITTLUNG, j)) + " "));
+				}
 				rInh.add("\n");
-				
+
 				rVerh.add(new Chunk("Wie sagte Ihnen ihr/sein Verhalten zu?"));
 				leerzeichenSetzen(rVerh);
-				for(int j=0; j<ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
-					rVerh.add(new Chunk(" "+String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.VORBEREITUNG, j))+" "));	
-				}	
+				for (int j = 0; j < ar.get(i).stimmenProRadioBtnVorbereitung.length; j++) {
+					rVerh.add(new Chunk(
+							" " + String.valueOf(ar.get(i).getStimmenProRadioButton(Frage.VORBEREITUNG, j)) + " "));
+				}
 				rVerh.add("\n");
-				
-				rBem.add(new Chunk("Bermerkungen zu: "+ar.get(i).name+"\n"));
+
+				rBem.add(new Chunk("Bermerkungen zu: " + ar.get(i).name + "\n"));
 				for (int j = 0; j < ar.get(i).bemerkungen.size(); j++) {
-					rBem.add(new Chunk(ar.get(i).bemerkungen.get(j)+"; "));
+					rBem.add(new Chunk(ar.get(i).bemerkungen.get(j) + "; "));
 				}
 				rBem.add("\n");
-				
+
 				rAlle.add(rVorb);
 				rVorb.clear();
-				
+
 				rAlle.add(rFach);
 				rFach.clear();
-				
+
 				rAlle.add(rEing);
 				rEing.clear();
-				
+
 				rAlle.add(rInh);
 				rInh.clear();
-				
+
 				rAlle.add(rVerh);
 				rVerh.clear();
-				
+
 				rAlle.add(rBem);
 				rBem.clear();
-				
-				
+
 			}
-			
-			
+
 			document.add(rAlle);
-			
+
 			document.close();
 
 		} catch (DocumentException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	void leerzeichenSetzen(Paragraph paragraph) {
-		while(paragraph.getContent().length()<69) {
+		while (paragraph.getContent().length() < 69) {
 			paragraph.add(" ");
 		}
 	}
-	
+
 	void leerzeichenSetzen(Phrase phrase) {
-		while(phrase.getContent().length()<69) {
+		while (phrase.getContent().length() < 69) {
 			phrase.add(" ");
 		}
 	}
