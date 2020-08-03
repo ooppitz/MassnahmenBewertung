3c0f50ae4b70f048e25f4fce0aeafbd33f92023d
Lou-isa <louisazuhause@gmail.com>
2020-08-03 10:25:53 +0200

    Controller-Interface hinzufügen

---
+++ src/main/java/de/azubiag/MassnahmenBewertung/UI/Controller.java
@@ -0,0 +1,7 @@
+package de.azubiag.MassnahmenBewertung.UI;
+
+public interface Controller {
+/*Interface zum alleinigen Zweck, alle Controller als Parameter einer Methode übergeben zu können. 
+ * Kann erweitert werden , muss aber nicht.
+ */
+}
