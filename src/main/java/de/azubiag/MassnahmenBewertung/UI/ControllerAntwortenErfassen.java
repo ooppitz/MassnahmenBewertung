3e0ab28cf3c710e32e011922562b7a2953be0165
Lou-isa <louisazuhause@gmail.com>
2020-08-05 15:38:41 +0200

    deleteActions() ausführen, wenn Umfrage ausgewertet wird

--- src/main/java/de/azubiag/MassnahmenBewertung/UI/ControllerAntwortenErfassen.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/UI/ControllerAntwortenErfassen.java
@@ -351,7 +351,10 @@ public class ControllerAntwortenErfassen implements Serializable, Controller {
 
 					int userAntwort = AlertMethoden.zeigeAlertJaNeinAbbrechen(AlertType.WARNING, "Auswerten",
 							"Eine Umfrage kann nur einmal ausgewertet werden. Jetzt auswerten?");
+					
 					if(userAntwort == AlertMethoden.JA) {
+						mainapp.deleteActions(tab, controller);
+						
 						MainApp.vonListeEntfernen(controller);// speichern bzw löschen, nachdem die Auswertung erstellt wurde
 //						mainapp.showTabAuswertungAnzeigen(eigenschaften, tab.getTabPane().getTabs().indexOf(tab), antwortListe);
 						String pdfOutputPfad = System.getenv("LOCALAPPDATA")+"\\MassnahmenBewertung\\UmfragenergebnissePDFs\\"+ eigenschaften.fragebogen_name+".pdf"; 
