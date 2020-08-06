75190c3e06613daae22a46c1a1ccb9fc6afccc1c
Luna <lunadragofelis@gmail.com>
2020-08-06 11:53:21 +0200

    Javadoc zu DemoTextGenerator hinzugefügt

--- src/main/java/de/azubiag/MassnahmenBewertung/tools/DemoTextGenerator.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/tools/DemoTextGenerator.java
@@ -2,6 +2,14 @@ package de.azubiag.MassnahmenBewertung.tools;
 
 import java.util.Random;
 
+/**
+ * randomly picks from a number of preset textstrings
+ * <p>
+ * Usage: call the {@link DemoTextGenerator#generate()} method
+ * 
+ * @author Luna
+ *
+ */
 public final class DemoTextGenerator {
 	
 	private static final String[] TEXTE = {
@@ -27,10 +35,19 @@ public final class DemoTextGenerator {
 	
 	private Random rng;
 	
+	/**
+	 * Creates a new {@link DemoTextGenerator}.
+	 */
 	public DemoTextGenerator() {
 		rng = new Random();
 	}
 	
+	/**
+	 * randomly picks from an internal array of {@link String}s
+	 * @return the picked {@link String}
+	 * 
+	 * @author Luna
+	 */
 	public String generate() {
 		return TEXTE[rng.nextInt(TEXTE.length)];
 	}
