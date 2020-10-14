c6bceaed7671e9f00908102d3ae30792241b9026
Filip Golanski <filip.golanski@googlemail.com>
2020-10-14 11:00:36 +0200

    Anwendung schließt sich, nachdem sich das Hauptfenster geschlossen hat

--- src/main/java/de/azubiag/MassnahmenBewertung/MassnahmenBewertung.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/MassnahmenBewertung.java
@@ -7,6 +7,7 @@ public class MassnahmenBewertung  {
 	public static void main(String[] args) {
 		
 		MainApp.main(args);
+		System.exit(0);	// ansonsten würde der Thread weiterlaufen, der abfrägt, ob ein Fragebogen schon im Internet zu finden ist
 	}
 
 
