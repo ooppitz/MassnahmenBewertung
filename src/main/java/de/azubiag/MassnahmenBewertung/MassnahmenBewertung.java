190d71c0ede1f0248c3b4e29e96f2e86b86b4146
Denis Bode <bode.denis123@gmail.com>
2020-10-20 12:57:07 +0100

    Gibt es classpath änderungen?

--- src/main/java/de/azubiag/MassnahmenBewertung/MassnahmenBewertung.java
+++ src/main/java/de/azubiag/MassnahmenBewertung/MassnahmenBewertung.java
@@ -7,7 +7,7 @@ public class MassnahmenBewertung  {
 	public static void main(String[] args) {
 		
 		MainApp.main(args);
-		System.exit(0);	// ansonsten würde der Thread weiterlaufen, der abfrägt, ob ein Fragebogen schon im Internet zu finden ist
+		System.exit(0);	// ansonsten würde der Thread weiterlaufen, der abfrägt, ob ein Fragebogen schon im Internet zu finden ist!
 	}
 
 
